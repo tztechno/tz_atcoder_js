f1daa09673da1107b014731c314f6f00eed8bfeb
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-23 19:58:28 +0900

    Create moveright.js

---
+++ moveright.js
@@ -0,0 +1,7 @@
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const N = input[0];
+console.log("0" + N.slice(0,3));
