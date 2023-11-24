8d85a4ad1256e17db5568df6e41b28f919d2f7d6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-24 11:51:59 +0900

    Create magnitude.js

---
+++ magnitude.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const [A, B] = input.split(' ').map(Number);
+console.log(Math.pow(32, A - B));
