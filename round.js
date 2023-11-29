aba7dc358cf56c2ce8edb789745bb57d98f10150
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-29 17:10:20 +0900

    Create round.js

---
+++ round.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const X = parseFloat(input);
+const a = parseInt(X * 10);
+
+if (a % 10 >= 5) {
+  console.log(parseInt(X) + 1);
+} else if (a % 10 <= 4) {
+  console.log(parseInt(X));
+}
