ddeaf895d2e9f25dd486211bd049fac31eec4d9b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-30 13:12:57 +0900

    Create qq.js

---
+++ qq.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const a=input[0]
+const b=input[2]
+const ans=parseInt(a)*parseInt(b)
+
+console.log(ans);
