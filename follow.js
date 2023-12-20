f72a4ae41a343a97c07158b5fb3c9a82c8758f36
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-20 12:37:11 +0900

    Create follow.js

---
+++ follow.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const [A, B] = input.split(' ').map(Number);
+const XB = 2*A+100
+
+console.log(XB-B);
