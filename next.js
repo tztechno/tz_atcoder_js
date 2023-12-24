b8c4d002e4381fa2a465d8c1ec12d341bdab059b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-24 21:46:38 +0900

    Update next.js

--- next.js
+++ next.js
@@ -1,6 +1,16 @@
 #ABC329_B
 #next
 
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n")[1]; 
+
+const C = Array.from(new Set(input.split(' ').map(Number))).sort((a, b) => b - a);
+console.log(C[1]);
+
+##################################
+  
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n")[1]; 
@@ -8,6 +18,5 @@ const input = fs.readFileSync(filePath).toString().split("\n")[1];
 const A = input.split(' ').map(Number);
 const B = new Set(A);
 const C = Array.from(B).sort((a, b) => b - a);
-
 console.log(C[1]);
 
