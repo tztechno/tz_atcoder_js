662652e688923f0e28d75533c76bf19982a2b17a
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-13 14:51:13 +0900

    Update lowerupper.js

--- lowerupper.js
+++ lowerupper.js
@@ -1,5 +1,24 @@
 //ABC192_B lowerupper
 
+###################################################################
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const S = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const n: number = S.length;
+
+for (let i = 0; i < n; i++) {
+    if ((i % 2 === 0 && S[i] !== S[i].toLowerCase()) || (i % 2 === 1 && S[i] !== S[i].toUpperCase())) {
+        console.log('No');
+        process.exit(0); // use process.exit(0) to exit the program
+    }
+}
+
+console.log('Yes');
+
+###################################################################
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const S = fs.readFileSync(filePath).toString().split("\n")[0];
@@ -20,3 +39,5 @@ if (U === U.toUpperCase() && L === L.toLowerCase()) {
 } else {
   console.log('No');
 }
+
+###################################################################
