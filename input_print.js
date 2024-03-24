7eb00d841d0701240f3b9ca5a52cfcdca1483a51
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-24 15:07:44 +0900

    Update input_print.js

--- input_print.js
+++ input_print.js
@@ -3,3 +3,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n")[0];
 
 console.log(input);
+
+
+################################################
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [n, k] = input[0].split(" ").map(Number); // Splitting and converting to numbers
+let L = input[1].split(" ").map(Number); // Splitting and converting to numbers
